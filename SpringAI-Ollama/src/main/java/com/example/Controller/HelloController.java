7fabf09b85ab364bcfe1583467d59da74afab0d0
Prajjwal Yadav <94003570+prajjwal77@users.noreply.github.com>
2025-02-28 21:40:07 +0530

    Add files via upload

---
+++ SpringAI-Ollama/src/main/java/com/example/Controller/HelloController.java
@@ -0,0 +1,36 @@
+package com.example.Controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
+public class HelloController {
+
+    private final ChatClient chatClient;
+
+    public HelloController(ChatClient.Builder chatClient) {
+        this.chatClient = chatClient.build();
+    }
+
+
+    @GetMapping("/{chat}")
+    public ResponseEntity<String> promptWithPathVariable(@PathVariable String chat) {
+        try {
+            String response = chatClient
+                    .prompt(chat)
+                    .call()
+                    .content();
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+        }
+    }
+
+}
